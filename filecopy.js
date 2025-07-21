8f078b0fa8fe7c1039101ecdfb7d2fc86eba2055
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-21 20:58:09 +0200

    Modified destinational.txt file

--- filecopy.js
+++ filecopy.js
@@ -4,11 +4,4 @@ fs.readFile('source.txt', 'utf8', (err, data) => {
   if (err) {
     return console.error("Error reading source file:", err);
   }
-
-  fs.writeFile('destination.txt', data, (err) => {
-    if (err) {
-      return console.error("Error writing destination file:", err);
-    }
-    console.log("File successfully copied!");
-  });
-});
+})
