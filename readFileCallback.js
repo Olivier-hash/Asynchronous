f66be86e5d63e81dbb2897e3ed9f1e1fa845e30a
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-21 20:59:26 +0200

    deleted readfilemodule file

--- readFileCallback.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('sample.txt', 'utf8', (err, data) => {
-  if (err) {
-    return console.error("Failed to read file:", err);
-  }
-  console.log("File content:", data);
-});
