5bcc2302a1fe68a446819f62a232b69e751835c4
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-23 22:30:27 +0200

    Added asynchronous function to catch error

--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -3,3 +3,28 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
 
+
+const sendEmail = async ( email,subject,text )=>{
+
+    try {
+        const Transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        })
+
+        await Transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to:email,
+            subject: subject,
+            text:text
+        })
+    } catch (error) {
+        console.log('error ocurred', error);
+        
+    }
+}
+
+module.exports = sendEmail
