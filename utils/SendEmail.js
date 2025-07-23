6c1f220d6ced0ea860742234bdc50079cf9d5df3
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-23 22:33:07 +0200

    Modified Email Sender service to yahoo

--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -8,7 +8,7 @@ const sendEmail = async ( email,subject,text )=>{
 
     try {
         const Transporter = nodemailer.createTransport({
-            service: 'gmail',
+            service: 'yahoo',
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
@@ -18,8 +18,7 @@ const sendEmail = async ( email,subject,text )=>{
         await Transporter.sendMail({
             from: process.env.EMAIL_USER,
             to:email,
-            subject: subject,
-            text:text
+            
         })
     } catch (error) {
         console.log('error ocurred', error);
